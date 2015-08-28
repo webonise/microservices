b5acbd4d9253e580be406b4718bdb5a93f27bbe6
Robert Fischer <robert.fischer@weboniselab.com>
2015-08-27 21:48:01 -0400

    Example of Facebook config; ignore actual Facebook config

---
+++ third-auth/config/facebook.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+  appId: FACEBOOK_APP_ID,
+  appSecret: FACEBOOK_APP_SECRET,
+  scopes: ['public_profile', 'user_friends', 'email']  // https://developers.facebook.com/docs/facebook-login/permissions/v2.4#categories
+};
